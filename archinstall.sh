9afdd669118fe6efc10156359d6e2a83a1d796f9
Matthew Pare <paretech@gmail.com>
2017-10-19 23:40:17 -0400

    Updated to include EFI partition and formatting.

--- archinstall.sh
+++ archinstall.sh
@@ -4,34 +4,47 @@ HOSTNAME="apollo"
 USERNAME="paretech"
 TIMEZONE="US/Eastern"
 LANGUAGE="en_US.UTF-8"
+
+# To determine DRIVE, inspect output of
+# $ lsblk
 DRIVE=/dev/sda
 MOUNT_PATH=/mnt
 USERSHELL=/bin/bash
 
-# Test to see if operating in a chrooted environment. See 
+# Test to see if operating in a chrooted environment. See
 # http://unix.stackexchange.com/questions/14345/how-do-i-tell-im-running-in-a-chroot
 # for more information.
 if [ "$(stat -c %d:%i /)" == "$(stat -c %d:%i /proc/1/root/.)" ]; then ### Not chrooted ###
-    
+
 # prepare disk
 sgdisk --zap-all ${DRIVE}
 sgdisk --set-alignment=2048 ${DRIVE}
 sgdisk --clear ${DRIVE}
 
+# Common Partitions Types
+#   8300 Linux filesystem
+#   8200 linux swap
+#   fd00 linux raid
+#   ef02 BIOS boot
+#
+# For more use 'sgdisk -L'.
+
 # create partitions
-sgdisk -n 1:0:+250M -t 1:8300 -c 1:"Boot" ${DRIVE} # partition 1 (Boot)
-sgdisk -n 2:0:+2G   -t 2:8200 -c 2:"Swap" ${DRIVE} # partition 2 (Swap)
-sgdisk -n 3:0:0     -t 3:8300 -c 3:"Arch" ${DRIVE} # partition 3 (Arch)
+sgdisk --new=1:0:+500M --typecode=1:ef00 --change-name=1:"EFI System Partition" ${DRIVE} # partition 1 (EFI)
+sgdisk --new=2:0:+250M --typecode=2:8300 --change-name=2:"Boot" ${DRIVE} # partition 2 (Boot)
+sgdisk --new=3:0:+2G   --typecode=3:8200 --change-name=3:"Swap" ${DRIVE} # partition 3 (Swap)
+sgdisk --new=4:0:0     --typecode=4:8300 --change-name=4:"root" ${DRIVE} # partition 4 (Arch)
 
 # format partitions
-mkfs.ext4 ${DRIVE}1
-mkswap    ${DRIVE}2
-mkfs.ext4 ${DRIVE}3
+mkfs.fat -F32 ${DRIVE}1
+mkfs.ext4 ${DRIVE}2
+mkswap    ${DRIVE}3
+mkfs.ext4 ${DRIVE}4
 
 # mount partitions
-mount ${DRIVE}3 ${MOUNT_PATH}
-mkdir ${MOUNT_PATH}/boot && mount ${DRIVE}1 ${MOUNT_PATH}/boot
-swapon ${DRIVE}2
+mount ${DRIVE}4 ${MOUNT_PATH}
+mkdir ${MOUNT_PATH}/boot && mount ${DRIVE}2 ${MOUNT_PATH}/boot
+swapon ${DRIVE}3
 
 # install base system
 pacstrap ${MOUNT_PATH} base base-devel
@@ -52,7 +65,7 @@ umount -R ${MOUNT_PATH}
 reboot
 fi ### END chroot check ###
 
-# Test to see if operating in a chrooted environment. See 
+# Test to see if operating in a chrooted environment. See
 # http://unix.stackexchange.com/questions/14345/how-do-i-tell-im-running-in-a-chroot
 # for more information.
 if [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]; then
